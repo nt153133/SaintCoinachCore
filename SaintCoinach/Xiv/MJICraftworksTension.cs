66b4e03547eef355be4d572361f88a32f46d8e96
nt153133 <52430037+nt153133@users.noreply.github.com>
2023-12-07 14:18:53 -0500

    More island stuff

---
+++ SaintCoinach/Xiv/MJICraftworksTension.cs
@@ -0,0 +1,12 @@
+﻿using SaintCoinach.Ex.Relational;
+
+namespace SaintCoinach.Xiv;
+
+public class MJICraftworksTension : XivRow
+{
+    public MJICraftworksTension(IXivSheet sheet, IRelationalRow sourceRow) : base(sheet, sourceRow)
+    {
+    }
+    
+    public byte MaxGroove => As<byte>("MaxGroove");
+}
--- SaintCoinach/Xiv/MJIItemPouch.cs
+++ SaintCoinach/Xiv/MJIItemPouch.cs
@@ -6,8 +6,19 @@ namespace SaintCoinach.Xiv
     {
         public Item Item => As<Item>("Item");
         
+        public byte Category => (byte)AsInt32("Category");
+        
+        public byte Crop => (byte)AsInt32("Crop");
+        
+        public byte Sort => As<byte>("Sort");
+        
         public MJIItemPouch(IXivSheet sheet, IRelationalRow sourceRow) : base(sheet, sourceRow)
         {
+        }
+        
+        public override string ToString()
+        {
+            return Item.Name;
         }
     }
 }
